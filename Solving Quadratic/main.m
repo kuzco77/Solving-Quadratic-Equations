e6aaac4fc337ec40f58b4b8c111cb653dc357cc6
Chu Nam Anh <khuyendaxoa1996@yahoo.com.vn>
2016-06-08 15:04:57 +0700

    viet lai toan bo

--- Solving Quadratic/main.m
+++ Solving Quadratic/main.m
@@ -1,17 +1,48 @@
 //
 //  main.m
-//  Solving Quadratic
+//  FirstiOS
 //
-//  Created by Chu Nam Anh on 6/8/16.
-//  Copyright © 2016 Chu Nam Anh. All rights reserved.
+//  Created by iTD on 6/7/16.
+//  Copyright © 2016 iTD. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+
+int diemtoan=10;
+void Diem()
+{
+    int diemtoan=10;
+    diemtoan++;
+}
+
+void Run()
+{
+    diemtoan++;
+}
+
+void Giaipt(float a,float b,float c)
+{
+    if (a==0)
+    {
+        if (b==0) printf("%f",-c/b);
+    }
+    
+    else
+    {
+        float delta = b*b - 4*a*c;
+        if (delta < 0) printf ("Khong co");
+        if (delta==0) printf ("%f",-b/(2*a));
+        if (delta>0) printf ("%f",(-b + sqrt(delta))/(2*a));
+    }
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
     }
+    int haha=10;
+    printf("%d",haha);
+    printf("Cha biet viet cai gi\n");
     return 0;
 }
