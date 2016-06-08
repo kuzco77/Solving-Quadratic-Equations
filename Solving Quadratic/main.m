fbc9ff2257455c9430b8d9c4051b4266c1480f4e
Chu Nam Anh <khuyendaxoa1996@yahoo.com.vn>
2016-06-08 15:02:07 +0700

    Initial Commit

---
+++ Solving Quadratic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Solving Quadratic
+//
+//  Created by Chu Nam Anh on 6/8/16.
+//  Copyright © 2016 Chu Nam Anh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
